27155bbea812ebf396903128fdaaa43c3bd71ab2
liujang <41243924+liujang@users.noreply.github.com>
2020-11-23 22:27:58 +0800

    Add files via upload

---
+++ foc.sh
@@ -0,0 +1,170 @@
+#!/usr/bin/env bash
+#Fly Over Cloud
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+sleep 3
+if [[ "$EUID" -ne 0 ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+    
+     if [[ $release = "ubuntu" || $release = "debian" ]]; then
+    PM='apt'
+  elif [[ $release = "centos" ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+  if [ $PM = 'apt' ] ; then
+    apt-get update -y
+    apt-get install vim curl git wget zip unzip python3 python3-pip git -y
+elif [ $PM = 'yum' ]; then
+    yum update -y
+    yum install vim curl git wget zip unzip python3 python3-pip git -y
+fi
+wget https://github.com/caddyserver/caddy/releases/download/v1.0.4/caddy_v1.0.4_linux_amd64.tar.gz && tar -xf caddy_v1.0.4_linux_amd64.tar.gz
+echo -e
+mv caddy /usr/bin/
+echo -e
+mkdir -p /usr/local/caddy/www/ && cd /usr/local/caddy/www/
+ echo -e "下面提供了15个不同的伪装网站模板，按对应的数字进行安装，安装之前可以查看网站demo:
+  ${GREEN}1. https://templated.co/intensify
+  ${GREEN}2. https://templated.co/binary
+  ${GREEN}3. https://templated.co/retrospect
+  ${GREEN}4. https://templated.co/spatial
+  ${GREEN}5. https://templated.co/monochromed
+  ${GREEN}6. https://templated.co/transit
+  ${GREEN}7. https://templated.co/interphase
+  ${GREEN}8. https://templated.co/ion
+  ${GREEN}9. https://templated.co/solarize
+  ${GREEN}10. https://templated.co/phaseshift
+  ${GREEN}11. https://templated.co/horizons
+  ${GREEN}12. https://templated.co/grassygrass
+  ${GREEN}13. https://templated.co/breadth
+  ${GREEN}14. https://templated.co/undeviating
+  ${GREEN}15. https://templated.co/lorikeet${NO_COLOR}
+  "
+  read -p "您输入你要安装的网站的数字:" aNum
+case $aNum in
+    1)wget -O web.zip --no-check-certificate https://templated.co/intensify/download
+    ;;
+    2)wget -O web.zip --no-check-certificate https://templated.co/binary/download
+    ;;
+    3)wget -O web.zip --no-check-certificate https://templated.co/retrospect/download
+    ;;
+    4)wget -O web.zip --no-check-certificate https://templated.co/spatial/download
+    ;;
+    5)wget -O web.zip --no-check-certificate https://templated.co/monochromed/download
+    ;;
+    6)wget -O web.zip --no-check-certificate https://templated.co/transit/download
+    ;;
+    7)wget -O web.zip --no-check-certificate https://templated.co/interphase/download
+    ;;
+    8)wget -O web.zip --no-check-certificate https://templated.co/ion/download
+    ;;
+    9)wget -O web.zip --no-check-certificate https://templated.co/solarize/download
+    ;;
+    10)wget -O web.zip --no-check-certificate https://templated.co/phaseshift/download
+    ;;
+    11)wget -O web.zip --no-check-certificate https://templated.co/horizons/download
+    ;;
+    12)wget -O web.zip --no-check-certificate https://templated.co/grassygrass/download
+    ;;
+    13)wget -O web.zip --no-check-certificate https://templated.co/breadth/download
+    ;;
+    14)wget -O web.zip --no-check-certificate https://templated.co/undeviating/download
+    ;;
+    15)wget -O web.zip --no-check-certificate https://templated.co/lorikeet/download
+    ;;
+    *)wget -O web.zip --no-check-certificate https://templated.co/intensify/download
+    ;;
+esac
+    unzip web.zip
+    mv web/* .
+echo -e
+read -p "输入您的域名:" domainname
+  echo "https://${domainname}:2333 {
+root /usr/local/caddy/www/
+timeouts none
+tls duyuanchao.me@gmail.com
+}" > /usr/local/caddy/Caddyfile
+cd ../
+caddy
+nohup caddy &
+send "\03" 
+cd
+echo -e
+git clone https://github.com/liujang/foc-.git
+echo -e
+cd foc-
+pip3 install -r requirements.txt
+sleep 5
+cp apiconfig.py userapiconfig.py && cp config.json user-config.json
+echo -e
+echo -e "
+ ${GREEN} 1.web
+ ${GREEN} 2.db
+ "
+ read -p "您输入你要的对接方式:" aNum
+if [ "$aNum" = "1" ];then
+read -p "请输入网站mukey:" key
+ echo "网站mukey为：${key}"
+ sleep 1
+ sed -i '17s/123/'${key}'/' userapiconfig.py
+ read -p "请输入节点序号:" node
+ echo "节点序号为：${node}"
+ sleep 1
+ sed -i '2s/0/'${node}'/' userapiconfig.py
+  cd
+  cd foc- && chmod +x run.sh && ./run.sh
+ elif [ "$aNum" = "2" ] ;then
+ sed -i '14s/modwebapi/glzjinmod/' userapiconfig.py
+ read -p "请输入数据库地址:" ip
+ echo "网站mukey为：${ip}"
+ sleep 1
+ sed -i '23s/127.0.0.1/'${ip}'/' userapiconfig.py
+ read -p "请输入数据库用户名:" user
+ echo "数据库用户名为：${user}"
+ sleep 1
+ sed -i '25s/ss/'${user}'/' userapiconfig.py
+ read -p "请输入数据库名:" db
+ echo "数据库名为：${db}"
+ sleep 1
+ sed -i '27s/shadowsocks/'${db}'/' userapiconfig.py
+ read -p "请输入数据库密码:" passwd
+ echo "数据库密码为：${passwd}"
+ sleep 1
+ sed -i '26s/ss/'${passwd}'/' userapiconfig.py
+ read -p "请输入节点序号:" node
+ echo "节点序号为：${node}"
+ sleep 1
+ sed -i '2s/0/'${node}'/' userapiconfig.py
+  cd
+  cd foc- && chmod +x run.sh && ./run.sh
+  else
+            echo "你他妈是猪吗，就两个数字给你选，你都选错，滚！！！"
+            fi
+echo "已经对接完成！！！ Fly Over Cloud牛逼。"
+echo "本脚本为foc的一键对接脚本，只适用于foc机场"
